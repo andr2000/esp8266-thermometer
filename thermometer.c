cc90ac41576bfc41f9fc76d31a7443648dbd4bc1
Oleksandr Andrushchenko <andr2000@gmail.com>
2018-11-28 20:13:12 +0200

    Move WiFi code into a separate module
    Signed-off-by: Oleksandr Andrushchenko <andr2000@gmail.com>

--- thermometer.c
+++ thermometer.c
@@ -8,52 +8,7 @@
 #include "debug.h"
 #include "rboot-ota.h"
 #include "user_config.h"
-
-#ifndef WIFI_SSID
-#error "WIFI_SSID must be defined"
-#endif
-#ifndef WIFI_PWD
-#error "WIFI_PWD must be defined"
-#endif
-
-static os_timer_t network_timer;
-
-void ICACHE_FLASH_ATTR network_wait_for_ip()
-{
-	struct ip_info ipconfig;
-
-	os_timer_disarm(&network_timer);
-	wifi_get_ip_info(STATION_IF, &ipconfig);
-	if (wifi_station_get_connect_status() == STATION_GOT_IP && ipconfig.ip.addr != 0) {
-		char page_buffer[40];
-		os_sprintf(page_buffer,"ip: %d.%d.%d.%d\r\n",IP2STR(&ipconfig.ip));
-		INFO(page_buffer);
-	} else {
-		char page_buffer[40];
-		os_sprintf(page_buffer,"network retry, status: %d\r\n",wifi_station_get_connect_status());
-		if(wifi_station_get_connect_status() == 3) wifi_station_connect();
-		INFO(page_buffer);
-		os_timer_setfn(&network_timer, (os_timer_func_t *)network_wait_for_ip, NULL);
-		os_timer_arm(&network_timer, 2000, 0);
-	}
-}
-
-void ICACHE_FLASH_ATTR wifi_config_station()
-{
-
-	struct station_config stationConf;
-
-	wifi_set_opmode(0x1);
-	stationConf.bssid_set = 0;
-	os_strncpy(stationConf.ssid, WIFI_SSID, os_strlen(stationConf.ssid));
-	os_strncpy(stationConf.password, WIFI_PWD, os_strlen(stationConf.password));
-	wifi_station_set_config(&stationConf);
-	INFO("wifi connecting...\r\n");
-	wifi_station_connect();
-	os_timer_disarm(&network_timer);
-	os_timer_setfn(&network_timer, (os_timer_func_t *)network_wait_for_ip, NULL);
-	os_timer_arm(&network_timer, 2000, 0);
-}
+#include "wifi.h"
 
 void ICACHE_FLASH_ATTR ShowIP()
 {
---
+++ wifi.h
@@ -0,0 +1,8 @@
+#ifndef __WIFI_H
+#define __WIFI_H
+
+#include <c_types.h>
+
+void ICACHE_FLASH_ATTR wifi_config_station();
+
+#endif /* __WIFI_H */
