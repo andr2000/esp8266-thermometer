4732b51bfb51b5efd6dcff995ab6aba0603cd4ea
Oleksandr Andrushchenko <andr2000@gmail.com>
2018-11-28 20:11:44 +0200

    Move OTA configurable parameters to common include
    Move OTA parameters to user_config.h which will hold
    all configurable parameters.
    
    Signed-off-by: Oleksandr Andrushchenko <andr2000@gmail.com>

--- user_config.h
+++ user_config.h
@@ -1,3 +1,13 @@
 #ifndef __USER_CONFIG_H
 #define __USER_CONFIG_H
-#endif
+
+// ota server details
+#define OTA_HOST "192.168.1.10"
+#define OTA_PORT 80
+#define OTA_ROM0 "rom0.bin"
+#define OTA_ROM1 "rom1.bin"
+// OTA_FILE is not required, but is part of the example
+// code for writing arbitrary files to flash
+#define OTA_FILE "file.bin"
+
+#endif /* __USER_CONFIG_H */
